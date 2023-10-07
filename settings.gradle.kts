2dafe815c7c6c23e84c9f502fc62f93b01144879
TDM <movvatarundeepchowdary99@gmail.com>
2023-10-06 22:17:04 -0500

    Created the project - Tarun Deep

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TaskMasterApplication_Group02"
+include(":app")
+ 
